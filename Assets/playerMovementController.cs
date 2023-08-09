75c7caf09a114e47e5dc90348d8f7683d1c69acc
Scott Lee <scottlee5455@outlook.com>
2023-08-08 19:29:00 -0500

    small speed update

--- Assets/playerMovementController.cs
+++ Assets/playerMovementController.cs
@@ -7,8 +7,8 @@ public class PlayerMovementController : MonoBehaviour
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    private float playerSpeed = 2.0f;
-    private float jumpHeight = 1.0f;
+    private float playerSpeed = 5.0f;
+    private float jumpHeight = 5.0f;
     private float gravityValue = -9.81f;
 
     private void Start()

dcdf63b91543813fd04e129d41972631eabe0728
Scott Lee <scottlee5455@outlook.com>
2023-08-10 21:09:50 -0500

    have now made it so that the player moves in the direction the camera faces

--- Assets/playerMovementController.cs
+++ Assets/playerMovementController.cs
@@ -24,25 +24,27 @@ public class PlayerMovementController : MonoBehaviour
             playerVelocity.y = 0f;
         }
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
+        // Get the forward and right vectors of the camera
+        Vector3 forward = Camera.main.transform.forward;
+        Vector3 right = Camera.main.transform.right;
 
-if (move != Vector3.zero)
-{
-    gameObject.transform.forward = move;
-}
-else if (controller.isGrounded)
-{
-    playerVelocity = Vector3.zero;
-}
+        // Get player input for movement
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
+        // Calculate the movement direction based on camera orientation
+        Vector3 moveDirection = (forward * moveVertical + right * moveHorizontal).normalized;
 
-        // Changes the height position of the player..
+        // Move the player using the calculated movement direction
+        controller.Move(moveDirection * Time.deltaTime * playerSpeed);
+
+        // Apply jump mechanics
         if (Input.GetButtonDown("Jump") && groundedPlayer)
         {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
         }
 
+        // Apply gravity and move player
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
     }

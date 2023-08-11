18be39b9e568c7c4a309071a2e4e6cfba885884f
Scott Lee <scottlee5455@outlook.com>
2023-08-10 20:20:23 -0500

    looking for smoother controls

--- Assets/playerCameraController.cs
+++ Assets/playerCameraController.cs
@@ -7,13 +7,19 @@ public class PlayerCameraController : MonoBehaviour
 {
     public float sensitivity = 2.0f;
     public float verticalSensitivity = 2.0f;
-    public float zoomSpeed = 5.0f; // Adjust this value for zoom speed
-    public float minFOV = 30.0f;  // Minimum field of view
-    public float maxFOV = 60.0f;  // Maximum field of view
+    public float zoomSpeed = 5.0f;
+    public float minFOV = 30.0f;
+    public float maxFOV = 60.0f;
     public string playerTag = "Player";
+    public float smoothing = 5.0f; // Smoothing factor for camera movement and rotation
+    public float verticalLimit = 80.0f; // Limit for vertical rotation
+    public KeyCode resetKey = KeyCode.R; // Key to reset camera
 
     private CinemachineFreeLook freeLookCamera;
     private Transform followTarget;
+    private Vector3 offset;
+
+    private bool mouseLookEnabled = true;
 
     private void Start()
     {
@@ -25,6 +31,8 @@ public class PlayerCameraController : MonoBehaviour
             followTarget = playerObject.transform;
             freeLookCamera.Follow = followTarget;
             freeLookCamera.LookAt = followTarget;
+
+            offset = transform.position - followTarget.position;
         }
         else
         {
@@ -37,13 +45,38 @@ public class PlayerCameraController : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
 
-        freeLookCamera.m_XAxis.Value += mouseX * sensitivity;
+        // Toggle mouse look
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            mouseLookEnabled = !mouseLookEnabled;
+            Cursor.lockState = mouseLookEnabled ? CursorLockMode.Locked : CursorLockMode.None;
+            Cursor.visible = !mouseLookEnabled;
+        }
 
-        float verticalInput = Input.GetAxis("Vertical");
-        freeLookCamera.m_YAxis.Value += verticalInput * verticalSensitivity;
+        if (mouseLookEnabled)
+        {
+            // Smoothly rotate the camera horizontally
+            freeLookCamera.m_XAxis.Value += mouseX * sensitivity * Time.deltaTime * smoothing;
+
+            // Smoothly rotate the camera vertically and apply limits
+            float verticalInput = -mouseY * verticalSensitivity * Time.deltaTime * smoothing;
+            float newVerticalRotation = freeLookCamera.m_YAxis.Value + verticalInput;
+            freeLookCamera.m_YAxis.Value = Mathf.Clamp(newVerticalRotation, -verticalLimit, verticalLimit);
+        }
 
         // Zoom using mouse scroll wheel input
         float scrollInput = Input.GetAxis("Mouse ScrollWheel");
         freeLookCamera.m_Lens.FieldOfView = Mathf.Clamp(freeLookCamera.m_Lens.FieldOfView - scrollInput * zoomSpeed, minFOV, maxFOV);
+
+        // Reset camera position with a key press
+        if (Input.GetKeyDown(resetKey))
+        {
+            freeLookCamera.m_XAxis.Value = 0.5f; // Reset horizontal rotation
+            freeLookCamera.m_YAxis.Value = 0.5f; // Reset vertical rotation
+        }
+
+        // Update camera position to maintain offset
+        Vector3 desiredPosition = followTarget.position + offset;
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * smoothing);
     }
 }
